df4bdb84915136032efe5fe03a11179b0377127d
Priyanka Garsole <priyankagarsole24@gmail.com>
2022-10-11 11:53:35 +0530

    Developer 4 commit

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 developer 1 has defined function 'sum'.
 developer 2 has added two print statements.
 developer 3 has defined function 'mul'.
+main has written the function 'div'.
+developer was added.
+developer4 was added.
+developer 4 has written the function 'sub'.
--- requirements.py
+++ requirements.py
@@ -1,19 +1,34 @@
 # This is a file stating all the requirements.
 # All the requirements here...
-def sum():
-    x=30
-    y=25
+
+print("/n")
+print("Python code written for Sum")
+def sum(x,y):
     z = x+y
     return z
-# print("The sum is:",sum)
-
-print("Python code written for sum")
-
+print("The sum is:",sum(30,20))
 print("SUM calculated successfully.")
 
+print("/n")
+print("Python code written for Multiplication")
 def mul(a,b):
     c = a*b
     return c
-
 print("The multiplication is:", mul(10,25))
+print("MUL calculated successfully.")
 
+print("/n")
+print("Python code written for Division")
+def div(x,y):
+    z = x/y
+    return z
+print("The division is:", div(25,10))
+print("DIV calculated successfully.")
+
+print("/n")
+print("Python code written for Subtraction.")
+def sub(m,n):
+    p = m-n
+    return p
+print("The subtraction is:", sub(96,57))
+print("SUB calculated successfully.")
